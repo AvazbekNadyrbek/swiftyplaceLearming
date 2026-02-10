951ba64469f6f59858f2670fd62a62f3020b8e77
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2026-02-10 11:47:34 +0100

    Initial Commit

---
+++ swiftyplaceLearming/swiftyplaceLearmingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftyplaceLearmingApp.swift
+//  swiftyplaceLearming
+//
+//  Created by Авазбек Надырбек уулу on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftyplaceLearmingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
