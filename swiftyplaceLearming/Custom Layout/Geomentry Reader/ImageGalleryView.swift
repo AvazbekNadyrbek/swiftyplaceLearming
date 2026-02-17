6afe121865eb3f00e0ccf2c13b13cefec6c8a1c0
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2026-02-17 22:13:51 +0100

    Here i am learned what is GEOMETRY READER and CONTAINERRELATIVEFRAME

---
+++ swiftyplaceLearming/Custom Layout/Geomentry Reader/ImageGalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageGalleryView.swift
+//  swiftyplaceLearming
+//
+//  Created by Авазбек Надырбек уулу on 2/17/26.
+//
+
+import SwiftUI
+
+struct ImageGalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageGalleryView()
+}
